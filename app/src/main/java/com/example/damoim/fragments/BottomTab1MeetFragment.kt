d2b341c7bd86b77802c06fa34244b631acecc15b
DK <spol@naver.com>
2022-09-07 21:49:06 +0900

    CreateActivity.kt 및 xml 작성 후 meetingAddBtn 온클릭 리스너 연결

--- app/src/main/java/com/example/damoim/fragments/BottomTab1MeetFragment.kt
+++ app/src/main/java/com/example/damoim/fragments/BottomTab1MeetFragment.kt
@@ -1,12 +1,16 @@
 package com.example.damoim.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
+import com.example.damoim.CreateActivity
+import com.example.damoim.MainActivity
 import com.example.damoim.R
 import com.example.damoim.adapters.ViewPagerFragmentStateAdapter
 import com.example.damoim.databinding.FragmentBottomTab1MeetBinding
@@ -35,13 +39,20 @@ class BottomTab1MeetFragment : BaseFragment() {
             tab.text = getTabTitle(position)
         }.attach()
 
+        setupEvents()
+        setValues()
         // 5. return Fragment Layout View
         return binding.root
+
     }
     override fun setupEvents() {
+        binding.meettingAddBtn.setOnClickListener {
+            startActivity(Intent(mContext,CreateActivity::class.java))
+        }
     }
 
     override fun setValues() {
+
     }
     // Tab & ViewPager 연동 및 Tab title 설정
     private fun getTabTitle(position: Int): String? {
