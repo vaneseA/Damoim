9cadd64b647e8350b191b11ccdded9096465f9b6
DK <spol@naver.com>
2022-09-16 18:48:44 +0900

    회원가입, 로그인 xml 완료
    파이어베이스 이미지로드 작업중
    *이미지 저장은 아직.

--- app/src/main/java/com/example/damoim/SignupActivity.kt
+++ app/src/main/java/com/example/damoim/SignupActivity.kt
@@ -2,10 +2,21 @@ package com.example.damoim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-    }
+
+        signUpBtn.setOnClickListener {
+            val email = emailEdt.text.toString()
+            val password = passwordEdt.text.toString()
+            val nickname = nickEdt.text.toString()
+
+            Log.d("SignupActivity","Email is: " + email)
+            Log.d("SignupActivity","Password: $password")
+        }
+        }
 }
