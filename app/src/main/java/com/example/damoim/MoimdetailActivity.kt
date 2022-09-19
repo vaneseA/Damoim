c760d563b51701fdc8ea4c6d6b69213eb33fe0c7
DK <spol@naver.com>
2022-09-19 18:24:10 +0900

    임시 커밋2

---
+++ app/src/main/java/com/example/damoim/MoimdetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.damoim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoimdetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moimdetail)
+    }
+}
